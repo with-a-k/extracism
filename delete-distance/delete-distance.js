904214e3f7c446e6c5ef44d57efe73183bd9d6ce
Erik Butcher <with_a_k@mac.com>
2017-07-07 14:10:02 -0600

    Implement a fully working solution

--- delete-distance/delete-distance.js
+++ delete-distance/delete-distance.js
@@ -1,40 +1,46 @@
-//This solution is unfinished, but it addresses every test case
-//except one ("percolate" vs "chocolate").
-
-function sortStringsByLength(str1, str2) {
-  if (str1.length > str2.length) {
-    return [str2, str1];
-  } else {
-    return [str1, str2];
-  }
-}
-
 /*
-/Takes two arrays of characters and finds the longest string that can
-/be made from characters the two have in common and in the same order.
-/The first is expected to be shorter.
+The deletion distance between "" and any string of length l is l.
+The deletion distance between "${same}a" and "${diff}a" is the deletion distance between same and diff.
+The deletion distance between "${same}a" and "${diff}b" is the deletion distance between same and diff + 1.
+Using these, we can generate a map of the distances between any two given strings' substrings and just return
+the value in the far lower right.
 */
-function findCommonString(chars1, chars2) {
-  var common = [];
-  var check = chars2;
-  chars1.forEach(function(char) {
-    if (check.includes(char)) {
-      common.push(char);
-      check = chars2.slice(chars2.findIndex(function(inchar) {
-        return inchar === char;
-      }));
-    }
-  });
-  console.log(`the common word is ${common.join('')}`);
-  return common.join('');
+
+function arrayOfZeros(length) {
+  var ours = [];
+  for (var x = 0; x < length; x++) {
+    ours.push(0);
+  }
+  return ours;
 }
 
 module.exports.compare = function(str1, str2) {
-  if (str1 === str2) {
-    return 0;
+  var backup;
+  if (str1.length < str2.length) {
+    backup = str1;
+    str1 = str2;
+    str2 = backup;
   }
-  var short = sortStringsByLength(str1, str2)[0].split('');
-  var long = sortStringsByLength(str1, str2)[1].split('');
-  var common = findCommonString(short, long);
-  return (short.length - common.length) + (long.length - common.length);
+
+  var s1l = str1.length;
+  var s2l = str2.length;
+  var pastMem = arrayOfZeros(s2l+1);
+  var presMem = arrayOfZeros(s2l+1);
+
+  for (var i = 0; i <= s1l; i++) {
+    for (var j = 0; j <= s2l; j++) {
+      if (i === 0) {
+        presMem[j] = j;
+      } else if (j === 0) {
+        presMem[j] = i;
+      } else if (str1[i-1] === str2[j-1]) {
+        presMem[j] = pastMem[j-1];
+      } else {
+        presMem[j] = 1 + Math.min(pastMem[j], presMem[j-1]);
+      }
+    }
+    pastMem = presMem;
+    presMem = arrayOfZeros(s2l+1);
+  }
+  return pastMem[s2l];
 }
